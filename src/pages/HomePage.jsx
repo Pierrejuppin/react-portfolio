c5624153aa4349578f4aa77e2d709613f4c4414a
Pierre Juppin <pierre.juppin@gmail.com>
2024-06-17 19:05:01 +0200

    fix: fix02

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
 
 return (
 
-<div className="min-h-screen">
+<div className="min-h-screen m-10">
         <div className="m-10 mx-auto max-w-sm p-6 rounded-lg shadow bg-gray-800 border-gray-700 ">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Développeur Web Full Stack</h5>
             <p className="font-normal text-gray-400">J'ai rejoint la Wild Code School en février 2024 pour devenir développeur web JavaScript full stack. Je cherche une entreprise pour une alternance à partir de septembre 2024 dans le cadre de mon titre professionnel bac+4. Découvrez mes projets pour en savoir plus sur mon parcours !</p>
